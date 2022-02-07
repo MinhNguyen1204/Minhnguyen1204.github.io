d9862418234ec49e997236aeb84499b72bb3b00f
MinhNguyen1204 <vmnguyen1204@gmail.com>
2022-02-07 12:05:27 +0700

    update 2022

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-	const navItem = document.querySelectorAll(".nav-item");
-	const mainMenu = document.querySelectorAll(".main-menu") || [];
-
 	const getPercentOfScrollpage = (currentScroll) => {
 		const html = document.querySelector("html");
 		if (currentScroll <= 0) return 0;
 		return (currentScroll / (html.scrollHeight - html.clientHeight)) * 100;
 	};
 	const handleScroll = () => {
-		const scrollDistance = document.querySelector("html")?.scrollTop;
+		const mainMenu = document.querySelectorAll(".main-menu");
 		if (!mainMenu?.length) return;
+
+		const scrollDistance = document.querySelector("html")?.scrollTop;
+
+		const navItem = document.querySelectorAll(".nav-item");
 		mainMenu.forEach((item, index) => {
 			if (
 				scrollDistance + 20 >= item.offsetTop &&
@@ -20,23 +21,12 @@ document.addEventListener("DOMContentLoaded", () => {
 				navItem[index]?.classList?.remove("active");
 			}
 		});
+
 		const currentPercentPage = getPercentOfScrollpage(scrollDistance);
+
 		const percentPage = document.querySelector(".percent-page");
 		if (percentPage) percentPage.style.width = `${currentPercentPage}%`;
 	};
 
-	const handleClickNavItem = (element, idx) => {
-		if (!mainMenu.length || !mainMenu[idx]) return;
-
-		mainMenu[idx].scrollIntoView({
-			behavior: "smooth",
-		});
-	};
 	document.addEventListener("scroll", handleScroll, true);
-
-	if (navItem.length) {
-		navItem.forEach((ele, idx) => {
-			ele.onclick = () => handleClickNavItem(ele, idx);
-		});
-	}
 });
